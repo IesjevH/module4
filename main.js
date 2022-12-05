22196ae4745fb0410da1e0804a31b7c4c4b0731b
Isis <isisvanhemert@gmail.com>
2022-12-05 10:54:19 +0100

    klaar

---
+++ main.js
@@ -0,0 +1,31 @@
+var voorNaam = 'Isis';
+var tussenVoegsel = ' van';
+var achterNaam = ' Hemert';
+
+var completeNaam = voorNaam + tussenVoegsel + achterNaam;
+
+
+voorNaam = "Isis";
+tussenVoegsel = "van";
+achterNaam = "Hemert";
+
+//alert(completeNaam);
+
+console.log(completeNaam);
+
+if (voorNaam=="Isis")
+{
+    console.log("voorNaam is Isis");
+}
+else 
+{
+    console.log("voorNaam is NIET Isis");
+}
+
+document.getElementById("weekend").innerHTML = completeNaam;
+
+function klikknop()
+{
+    document.getElementById("weekend").innerHTML += "<br>"+completeNaam;
+
+}
