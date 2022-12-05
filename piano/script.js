7720e7ea5d82444f526a6fb553b7f7b019ae265d
Isis <isisvanhemert@gmail.com>
2022-12-05 11:22:19 +0100

    wekekend !!!!!!!!!!!!

---
+++ piano/script.js
@@ -0,0 +1,17 @@
+document.getElementById("c").onclick = function(){ 
+	var c_toets = document.getElementById("audio_c"); 
+	c_toets.play(); 
+}
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+
+document.getElementById("f").onclick = function(){
+	var f_toets = document.getElementById("audio_f");
+	f_toets.play();
+}
