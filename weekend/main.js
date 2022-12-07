700fb00b30cd21daacdfa562a379e6840ecc576e
Isis <isisvanhemert@gmail.com>
2022-12-07 10:32:10 +0100

    weekend

---
+++ weekend/main.js
@@ -0,0 +1,12 @@
+
+var beer ="<img src=./walk-polar-bear.gif>"
+
+function klikknop()
+{
+    document.getElementById("polarbear").innerHTML += "<img src=./walk-polar-bear.gif>";
+}
+
+
+console.log(beer)
+
+beer = "<img src=./walk-polar-bear.gif>";
