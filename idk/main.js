08e219120a56374fd79f1679089d2023f347b417
Isis <113422725+IesjevH@users.noreply.github.com>
2022-12-21 19:13:05 +0100

    ..

---
+++ idk/main.js
@@ -0,0 +1,10 @@
+var beer ="<img src=./walk-polar-bear.gif>"
+
+function klikknop()
+{
+    document.getElementById("polarbear").innerHTML += "<img src=./walk-polar-bear.gif>";
+}
+
+console.log(beer)
+
+beer = "<img src=./walk-polar-bear.gif>";
